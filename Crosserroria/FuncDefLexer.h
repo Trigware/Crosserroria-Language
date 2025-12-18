40f020e5c8542b6b09278b04582c0c33845bbf2e
Trigware <angryhonzik@gmail.com>
2025-12-18 20:05:56 +0100

    Added lexer support for loops

---
+++ Crosserroria/FuncDefLexer.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "Lexer.h"
+#include <optional>
+
+enum class InstructionType {
+	Unknown,
+	PlainExpression,
+	Declaration,
+	Assignment,
+	Conditional,
+	LoopStatement,
+	LoopControlFlow,
+	ReturnStatement,
+};
+
+enum class ConditionalType {
+	Unknown,
+	IfStatement,
+	ElifStatement,
+	ElseStatement
+};
+
+struct FunctionLevelInstruction {
+	FunctionLevelInstruction();
+	Expression underlyingExpression;
+	InstructionType instructionType = InstructionType::Unknown;
+	int nestingLevel = 0;
+	std::string variableName = "", indexVariableName = "";
+	DataType variableDeclarationType;
+	bool isDeclaredVariableConstant = false;
+	bool potentiallyEncounteredAssignmentSymbol = false, encounteredAssignment = false, isUninitialized = false, isWrappedInLoop = false, isInLoopIndexVariableName = false;
+	ConditionalType conditionalType;
+	operator std::string() const;
+};
